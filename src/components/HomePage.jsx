29fc5fd81a4ff42c0e1b2cbdb9a17a4183d37fac
Alex Jenkinson <alexjenkinson@ideasquared.co.uk>
2024-03-07 20:11:13 +0000

    HTML/CSS tidy

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
 
 const HomePage = () => {
 	return (
-		<div>
-			<h1>Welcome to My Website</h1>
-			<p>This is the homepage of my website.</p>
-			<p>Feel free to explore!</p>
-		</div>
+		<Container fluid>
+			<div>
+				<h1>Welcome to My Website</h1>
+				<p>This is the homepage of my website.</p>
+				<p>Feel free to explore!</p>
+			</div>
+		</Container>
 	);
 };
 
