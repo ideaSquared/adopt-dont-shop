4681e9cd597f3826b3dd3b98f7d7b50baad8db06
Alex Jenkinson <alexjenkinson@ideasquared.co.uk>
2024-03-29 10:58:16 +0000

    Conditional renderng for staff

--- src/components/RescueProfile.jsx
+++ src/components/RescueProfile.jsx
@@ -299,6 +299,7 @@ const RescueProfile = () => {
 
 	// Check for permissions
 	const canEditRescueInfo = userPermissions.includes('edit_rescue_info');
+	const canViewStaff = userPermissions.includes('view_staff');
 	const canAddStaff = userPermissions.includes('add_staff');
 	const canEditStaff = userPermissions.includes('edit_staff');
 	const canVerifyStaff = userPermissions.includes('verify_staff');
@@ -417,165 +418,173 @@ const RescueProfile = () => {
 
 			<hr />
 
-			<h2>Staff Members</h2>
-			<Button
-				variant='primary'
-				className='mb-3'
-				onClick={() => setShowAddStaffModal(true)}
-				disabled={!canAddStaff}
-			>
-				Add Staff
-			</Button>
-
-			<Table striped bordered hover>
-				<thead>
-					<tr>
-						<th>Staff Email</th>
-						{uniquePermissions.map((permission, index) => (
-							<th key={index}>{permission}</th>
-						))}
-						<th>Actions</th>
-					</tr>
-				</thead>
-				<tbody>
-					{rescueProfile.staff.map((staff) => (
-						<tr key={staff.userId._id}>
-							<td>{staff.userId.email}</td>
-							{uniquePermissions.map((permission) => (
-								<td key={`${staff.userId._id}-${permission}`}>
-									<Form.Check
-										type='checkbox'
-										checked={staff.permissions.includes(permission)}
-										onChange={(e) =>
-											handlePermissionChange(
-												staff.userId._id,
-												permission,
-												e.target.checked
-											)
-										}
-										disabled={staff.userId._id === userId || !canEditStaff} // Disable if this staff is the current user
-									/>
-								</td>
-							))}
-
-							<td>
-								{staff.verifiedByRescue ? (
-									<Button variant='info' disabled={true}>
-										Verified
-									</Button>
-								) : (
-									<Button
-										variant='warning'
-										onClick={() => handleVerifyStaff(staff.userId._id)}
-										disabled={!canVerifyStaff}
-									>
-										Verify Staff
-									</Button>
-								)}
-							</td>
-							<td>
-								<Button
-									variant='danger'
-									onClick={() => handleRemoveStaff(staff.userId._id)}
-									disabled={!canDeleteStaff}
-								>
-									Remove Staff
-								</Button>
-							</td>
-						</tr>
-					))}
-				</tbody>
-			</Table>
-			<PaginationControls
-				currentPage={currentPage}
-				totalPages={totalPages}
-				onChangePage={setCurrentPage}
-			/>
-			<Modal
-				show={showAddStaffModal}
-				onHide={() => setShowAddStaffModal(false)}
-			>
-				<Modal.Header closeButton>
-					<Modal.Title>Add New Staff Member</Modal.Title>
-				</Modal.Header>
-				<Modal.Body>
-					<Tabs defaultActiveKey='newUser' id='addUserTab'>
-						<Tab eventKey='newUser' title='Add a New User'>
-							<AlertComponent
-								type={'info'}
-								message={'This will create a new user who will need to login'}
-								hideCloseButton={true}
-							/>
-							<Form>
-								<Form.Group className='mb-3'>
-									<Form.Label>First name</Form.Label>
-									<Form.Control
-										type='text'
-										placeholder='Enter first name'
-										value={newStaff.firstName}
-										onChange={(e) =>
-											setNewStaff({ ...newStaff, firstName: e.target.value })
-										}
-									/>
-								</Form.Group>
-								<Form.Group className='mb-3'>
-									<Form.Label>Email address</Form.Label>
-									<Form.Control
-										type='email'
-										placeholder='Enter email'
-										value={newStaff.email}
-										onChange={(e) =>
-											setNewStaff({ ...newStaff, email: e.target.value })
-										}
-									/>
-								</Form.Group>
-								<Form.Group className='mb-3'>
-									<Form.Label>Password</Form.Label>
-									<Form.Control
-										type='password'
-										placeholder='Password'
-										value={newStaff.password}
-										onChange={(e) =>
-											setNewStaff({ ...newStaff, password: e.target.value })
-										}
-									/>
-								</Form.Group>
-							</Form>
-						</Tab>
-						<Tab eventKey='existingUser' title='Add an Existing User'>
-							<AlertComponent
-								type={'info'}
-								message={'This will add an already signed up user as staff'}
-								hideCloseButton={true}
-							/>
-							<Form>
-								<Form.Group className='mb-3'>
-									<Form.Label>Email address</Form.Label>
-									<Form.Control
-										type='email'
-										placeholder='Enter existing user email'
-									/>
-								</Form.Group>
-							</Form>
-						</Tab>
-					</Tabs>
-				</Modal.Body>
-				<Modal.Footer>
-					<Button
-						variant='secondary'
-						onClick={() => setShowAddStaffModal(false)}
-					>
-						Close
-					</Button>
+			{canViewStaff ? (
+				<div>
+					<h2>Staff Members</h2>
 					<Button
 						variant='primary'
-						onClick={handleAddStaff}
+						className='mb-3'
+						onClick={() => setShowAddStaffModal(true)}
 						disabled={!canAddStaff}
 					>
-						Add staff
+						Add Staff
 					</Button>
-				</Modal.Footer>
-			</Modal>
+					<Table striped bordered hover>
+						<thead>
+							<tr>
+								<th>Staff Email</th>
+								{uniquePermissions.map((permission, index) => (
+									<th key={index}>{permission}</th>
+								))}
+								<th>Actions</th>
+							</tr>
+						</thead>
+						<tbody>
+							{rescueProfile.staff.map((staff) => (
+								<tr key={staff.userId._id}>
+									<td>{staff.userId.email}</td>
+									{uniquePermissions.map((permission) => (
+										<td key={`${staff.userId._id}-${permission}`}>
+											<Form.Check
+												type='checkbox'
+												checked={staff.permissions.includes(permission)}
+												onChange={(e) =>
+													handlePermissionChange(
+														staff.userId._id,
+														permission,
+														e.target.checked
+													)
+												}
+												disabled={staff.userId._id === userId || !canEditStaff} // Disable if this staff is the current user
+											/>
+										</td>
+									))}
+
+									<td>
+										{staff.verifiedByRescue ? (
+											<Button variant='info' disabled={true}>
+												Verified
+											</Button>
+										) : (
+											<Button
+												variant='warning'
+												onClick={() => handleVerifyStaff(staff.userId._id)}
+												disabled={!canVerifyStaff}
+											>
+												Verify Staff
+											</Button>
+										)}
+									</td>
+									<td>
+										<Button
+											variant='danger'
+											onClick={() => handleRemoveStaff(staff.userId._id)}
+											disabled={!canDeleteStaff}
+										>
+											Remove Staff
+										</Button>
+									</td>
+								</tr>
+							))}
+						</tbody>
+					</Table>
+					<PaginationControls
+						currentPage={currentPage}
+						totalPages={totalPages}
+						onChangePage={setCurrentPage}
+					/>
+					<Modal
+						show={showAddStaffModal}
+						onHide={() => setShowAddStaffModal(false)}
+					>
+						<Modal.Header closeButton>
+							<Modal.Title>Add New Staff Member</Modal.Title>
+						</Modal.Header>
+						<Modal.Body>
+							<Tabs defaultActiveKey='newUser' id='addUserTab'>
+								<Tab eventKey='newUser' title='Add a New User'>
+									<AlertComponent
+										type={'info'}
+										message={
+											'This will create a new user who will need to login'
+										}
+										hideCloseButton={true}
+									/>
+									<Form>
+										<Form.Group className='mb-3'>
+											<Form.Label>First name</Form.Label>
+											<Form.Control
+												type='text'
+												placeholder='Enter first name'
+												value={newStaff.firstName}
+												onChange={(e) =>
+													setNewStaff({
+														...newStaff,
+														firstName: e.target.value,
+													})
+												}
+											/>
+										</Form.Group>
+										<Form.Group className='mb-3'>
+											<Form.Label>Email address</Form.Label>
+											<Form.Control
+												type='email'
+												placeholder='Enter email'
+												value={newStaff.email}
+												onChange={(e) =>
+													setNewStaff({ ...newStaff, email: e.target.value })
+												}
+											/>
+										</Form.Group>
+										<Form.Group className='mb-3'>
+											<Form.Label>Password</Form.Label>
+											<Form.Control
+												type='password'
+												placeholder='Password'
+												value={newStaff.password}
+												onChange={(e) =>
+													setNewStaff({ ...newStaff, password: e.target.value })
+												}
+											/>
+										</Form.Group>
+									</Form>
+								</Tab>
+								<Tab eventKey='existingUser' title='Add an Existing User'>
+									<AlertComponent
+										type={'info'}
+										message={'This will add an already signed up user as staff'}
+										hideCloseButton={true}
+									/>
+									<Form>
+										<Form.Group className='mb-3'>
+											<Form.Label>Email address</Form.Label>
+											<Form.Control
+												type='email'
+												placeholder='Enter existing user email'
+											/>
+										</Form.Group>
+									</Form>
+								</Tab>
+							</Tabs>
+						</Modal.Body>
+						<Modal.Footer>
+							<Button
+								variant='secondary'
+								onClick={() => setShowAddStaffModal(false)}
+							>
+								Close
+							</Button>
+							<Button
+								variant='primary'
+								onClick={handleAddStaff}
+								disabled={!canAddStaff}
+							>
+								Add staff
+							</Button>
+						</Modal.Footer>
+					</Modal>
+				</div>
+			) : null}
 		</Container>
 	);
 };
