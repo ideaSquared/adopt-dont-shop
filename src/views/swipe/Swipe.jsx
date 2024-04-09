1278db441374ea9a0b672e157dcb6b9a4a3d3582
Alex Jenkinson <alexjenkinson@ideasquared.co.uk>
2024-04-09 19:11:55 +0100

    Rebuilding swipe

---
+++ src/views/swipe/Swipe.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Container, Card, Button, Badge } from 'react-bootstrap';
+import './Swipe.css'; // Make sure to update this file with new styles.
+
+const Swipe = ({ item }) => {
+	const images = item.images;
+	const basePath = `${import.meta.env.VITE_API_IMAGE_BASE_URL}/uploads/`;
+	const fallbackImage = 'https://placehold.it/500';
+	const imageSrc =
+		images && images.length > 0
+			? `${basePath}${images[0]}`
+			: `${fallbackImage}`;
+
+	return (
+		<div className='d-flex align-items-center justify-content-center swipe-screen'>
+			<Card
+				className='swipe-card'
+				style={{ backgroundImage: `url(${imageSrc})` }}
+			>
+				<Card.Body className='swipe-overlay d-flex flex-column'>
+					<div className='d-flex justify-content-between'>
+						<Card.Title>{item.petName}</Card.Title>
+						<div className='badge-container d-flex align-items-start'>
+							<Badge bg='secondary'>Age: {item.age}</Badge>{' '}
+							<Badge bg='info'>{item.gender}</Badge>{' '}
+							<Badge bg={item.status === 'Adopted' ? 'success' : 'warning'}>
+								{item.status}
+							</Badge>
+						</div>
+					</div>
+					<Card.Text>{item.shortDescription}</Card.Text>
+					<div className='action-buttons mt-auto'>
+						<Button variant='outline-danger' className='rounded-circle dislike'>
+							Dislike
+						</Button>
+						<Button variant='outline-primary' className='rounded-circle love'>
+							Love
+						</Button>
+						<Button variant='outline-success' className='rounded-circle like'>
+							Like
+						</Button>
+					</div>
+				</Card.Body>
+			</Card>
+		</div>
+	);
+};
+
+export default Swipe;
--- src/views/swipe/SwipeContainer.jsx
+++ src/views/swipe/SwipeContainer.jsx
@@ -1,28 +1,20 @@
 import React, { useState } from 'react';
 import { Card } from 'react-bootstrap';
-import SwipeItem from './SwipeItem';
-import useFetchUnratedPets from '../../hooks/useFetchUnratedPets';
-import { useAuth } from '../../contexts/AuthContext';
-import { postRating } from '../../services/RatingService';
-import { useLoginRedirect } from '../../hooks/useLoginRedirect';
+import SwipeItem from '../../../_archive/SwipeItem';
+import useFetchUnratedPets from '../../hooks/useFetchUnratedPets'; // Adjust the path as necessary
+import { useAuth } from '../../contexts/AuthContext'; // Adjust the path as necessary
+import { postRating } from '../../services/RatingService'; // Adjust the path as necessary
+import Swipe from './Swipe';
 
-const SwipeContainer = ({ ratingSource, onModel }) => {
+const SwipeContainer = () => {
 	const [currentIndex, setCurrentIndex] = useState(0);
-	const { pets, isLoading, error, noUnratedPets } = useFetchUnratedPets();
+	const { pets, isLoading, error } = useFetchUnratedPets();
 	const { userId } = useAuth().authState;
-
-	useLoginRedirect();
 
 	const handleSwipe = async (direction) => {
 		if (currentIndex >= pets.length) return;
 		const ratingType = direction === 'left' ? 'dislike' : 'like';
-		await postRating(
-			pets[currentIndex]._id,
-			ratingType,
-			ratingSource,
-			onModel,
-			userId
-		);
+		await postRating(pets[currentIndex]._id, ratingType, userId);
 		setCurrentIndex((prevIndex) => prevIndex + 1);
 	};
 
@@ -38,25 +30,21 @@ const SwipeContainer = ({ ratingSource, onModel }) => {
 				<Card.Body>Error: {error}</Card.Body>
 			</Card>
 		);
-	if (noUnratedPets)
+	if (pets.length === 0)
 		return (
 			<Card>
-				<Card.Body>
-					Sorry! There are no more pets for you to rate at this moment.
-				</Card.Body>
+				<Card.Body>No more items to swipe!</Card.Body>
 			</Card>
 		);
 
 	return (
-		<div>
-			{currentIndex < pets.length ? (
-				<SwipeItem item={pets[currentIndex]} onSwipe={handleSwipe} />
-			) : (
-				<Card>
-					<Card.Body>No more items to swipe!</Card.Body>
-				</Card>
-			)}
-		</div>
+		<Swipe item={pets[currentIndex]} />
+		// <SwipeItem
+		// 	item={pets[currentIndex]}
+		// 	onSwipe={handleSwipe}
+		// 	currentIndex={currentIndex}
+		// 	petsLength={pets.length}
+		// />
 	);
 };
 
