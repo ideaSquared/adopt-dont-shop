f5f11319c76c3841a6a1c004aa0369d4d4c7a148
Alex Jenkinson <alexjenkinson@ideasquared.co.uk>
2024-04-17 17:56:08 +0100

    Adding admin redirect

--- src/views/admin/Dashboard.jsx
+++ src/views/admin/Dashboard.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
 import Charts from './Charts';
+import { useAdminRedirect } from '../../hooks/useAdminRedirect';
 
 const Dashboard = () => {
+	useAdminRedirect();
+
 	return (
 		<Container>
 			<Charts />
