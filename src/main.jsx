5d8a7c89fa6e2125c207398ee09894e196786dc2
Alex Jenkinson <alexjenkinson@ideasquared.co.uk>
2024-04-21 12:47:10 +0100

    🔧Refactored main.jsx to allow React 18 to work.

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { AuthProvider } from './contexts/AuthContext';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './app/App';
@@ -8,11 +8,15 @@ import axios from 'axios';
 axios.defaults.baseURL = import.meta.env.REACT_APP_API_BASE_URL;
 axios.defaults.withCredentials = true;
 
-ReactDOM.render(
+// Select the root container
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+// Initial render using React 18's createRoot
+root.render(
 	<React.StrictMode>
 		<AuthProvider>
 			<App />
 		</AuthProvider>
-	</React.StrictMode>,
-	document.getElementById('root')
+	</React.StrictMode>
 );
