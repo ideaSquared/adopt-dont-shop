fdf03ce274e6cb9795709c0516a64a88e3d17a6d
Alex Jenkinson <alexjenkinson@ideasquared.co.uk>
2024-06-01 11:29:56 +0100

    Moved logs to types

--- src/components/tables/LogsTable.tsx
+++ src/components/tables/LogsTable.tsx
@@ -1,63 +1,49 @@
 import React from 'react';
 import PaginationControls from '../common/PaginationControls';
 import StatusBadge from '../common/StatusBadge';
-
-interface Log {
-	_id: string;
-	timestamp: string;
-	level: string;
-	service: string;
-	message: string;
-}
-
-interface LogsTableProps {
-	currentLogs: Log[];
-	currentPage: number;
-	totalPages: number;
-	setCurrentPage: (page: number) => void;
-}
+import { LogsTableProps } from '../../types/logsTable';
 
 const LogsTable: React.FC<LogsTableProps> = ({
-	currentLogs,
-	currentPage,
-	totalPages,
-	setCurrentPage,
+    currentLogs,
+    currentPage,
+    totalPages,
+    setCurrentPage,
 }) => {
-	return (
-		<div>
-			<table className='table-auto w-full'>
-				<thead>
-					<tr>
-						<th className='border px-4 py-2'>Timestamp</th>
-						<th className='border px-4 py-2'>Level</th>
-						<th className='border px-4 py-2'>Service</th>
-						<th className='border px-4 py-2'>Message</th>
-					</tr>
-				</thead>
-				<tbody>
-					{currentLogs.map((log) => (
-						<tr key={log._id} className='hover:bg-gray-100'>
-							<td className='border px-4 py-2'>
-								{new Date(log.timestamp).toLocaleString()}
-							</td>
-							<td className='border px-4 py-2'>
-								<StatusBadge type='loglevel' value={log.level} />
-							</td>
-							<td className='border px-4 py-2'>
-								<StatusBadge type='logservice' value={log.service} />
-							</td>
-							<td className='border px-4 py-2'>{log.message}</td>
-						</tr>
-					))}
-				</tbody>
-			</table>
-			<PaginationControls
-				currentPage={currentPage}
-				totalPages={totalPages}
-				onChangePage={setCurrentPage}
-			/>
-		</div>
-	);
+    return (
+        <div>
+            <table className='table-auto w-full'>
+                <thead>
+                    <tr>
+                        <th className='border px-4 py-2'>Timestamp</th>
+                        <th className='border px-4 py-2'>Level</th>
+                        <th className='border px-4 py-2'>Service</th>
+                        <th className='border px-4 py-2'>Message</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {currentLogs.map((log) => (
+                        <tr key={log._id} className='hover:bg-gray-100'>
+                            <td className='border px-4 py-2'>
+                                {new Date(log.timestamp).toLocaleString()}
+                            </td>
+                            <td className='border px-4 py-2'>
+                                <StatusBadge type='loglevel' value={log.level} />
+                            </td>
+                            <td className='border px-4 py-2'>
+                                <StatusBadge type='logservice' value={log.service} />
+                            </td>
+                            <td className='border px-4 py-2'>{log.message}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+            <PaginationControls
+                currentPage={currentPage}
+                totalPages={totalPages}
+                onChangePage={setCurrentPage}
+            />
+        </div>
+    );
 };
 
 export default LogsTable;
