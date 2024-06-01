95114bc364cdec791475a7a40fb83d193c601683
Alex Jenkinson <alexjenkinson@ideasquared.co.uk>
2024-06-01 11:30:01 +0100

    Moved ratings to types

---
+++ src/types/rating.d.ts
@@ -0,0 +1,11 @@
+// src/types/rating.d.ts
+
+export interface Rating {
+	id: string;
+	name: string;
+	adopter_first_name: string;
+	adopter_last_name: string;
+	rating_type: string;
+	pet_id: string;
+	userid: string;
+}
