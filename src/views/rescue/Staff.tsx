f13ad5ca6d59bda65223de2bf297db124140e8cc
Alex Jenkinson <alexjenkinson@ideasquared.co.uk>
2024-06-01 21:01:47 +0100

    Typo fixed

--- src/views/rescue/Staff.tsx
+++ src/views/rescue/Staff.tsx
@@ -166,7 +166,7 @@ const Staff: React.FC<StaffProps> = ({ rescueProfile, userPermissions }) => {
 									onClick={() => setShowAddStaffSidebar(true)}
 									className='inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500'
 								>
-									Add Pet
+									Add Staff
 								</button>
 							</div>
 						)}
