178684eaf38360ef88a8371ace01dbb63b6b9dbc
Alex Jenkinson <alexjenkinson@ideasquared.co.uk>
2024-06-23 15:54:27 +0100

    Added a local .env roue bypass

--- src/views/dashboard/PrivateRoute.tsx
+++ src/views/dashboard/PrivateRoute.tsx
@@ -11,6 +11,12 @@ const PrivateRoute: React.FC<PrivateRouteProps> = ({
 }) => {
 	const { authState } = useAuth();
 
+	const bypassAuth = import.meta.env.VITE_DEV_BYPASS_AUTH === 'true';
+
+	if (bypassAuth) {
+		return <Outlet />;
+	}
+
 	if (!authState.isLoggedIn || (!authState.isRescue && !authState.isAdmin)) {
 		return <Navigate to={redirectPath} replace />;
 	}
