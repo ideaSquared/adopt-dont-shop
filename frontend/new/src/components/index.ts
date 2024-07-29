ecb0c56825d80c5ef75e02842de511418e68decb
Alex Jenkinson <alexjenkinson@ideasquared.co.uk>
2024-07-29 21:24:27 +0100

    Fixing the comment

--- frontend/new/src/components/index.ts
+++ frontend/new/src/components/index.ts
@@ -1,4 +1,4 @@
-// components/index.ts
+// src/components/index.ts
 export { default as Button } from './Button';
 export { default as TextInput } from './TextInput';
 export { default as SelectInput } from './SelectInput';
