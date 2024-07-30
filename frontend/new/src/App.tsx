7cea4b11ed278e7fea2ca723e607a7eb2c187484
Alex Jenkinson <alexjenkinson@ideasquared.co.uk>
2024-07-30 20:47:48 +0100

    Add routes

--- frontend/new/src/App.tsx
+++ frontend/new/src/App.tsx
@@ -2,15 +2,16 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
-import Home from './pages/Home';
-import Users from './pages/Users';
-import Login from './pages/user/Login';
+import Home from '@adoptdontshop/pages/Home';
+import Users from '@adoptdontshop/pages/Users';
+import Login from '@adoptdontshop/pages/user/Login';
 import CreateAccount from '@adoptdontshop/pages/user/CreateAccount';
 import ForgotPassword from '@adoptdontshop/pages/user/ForgotPassword';
 import { Header } from '@adoptdontshop/components';
 import ResetPassword from '@adoptdontshop/pages/user/ResetPassword';
 import { lightTheme, darkTheme } from './styles/theme';
 import GlobalStyles from './styles/GlobalStyles';
+import Settings from '@adoptdontshop/pages/user/Settings';
 
 const App: React.FC = () => {
 	const [isDarkMode, setIsDarkMode] = useState(false);
@@ -31,6 +32,7 @@ const App: React.FC = () => {
 					<Route path='/create-account' element={<CreateAccount />} />
 					<Route path='/forgot-password' element={<ForgotPassword />} />
 					<Route path='/reset-password' element={<ResetPassword />} />
+					<Route path='/settings' element={<Settings />} />
 				</Routes>
 			</Router>
 		</ThemeProvider>
