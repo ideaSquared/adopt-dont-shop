c1cb7f53e268906efbb472afd3d321c4016c7930
Alex Jenkinson <alexjenkinson@ideasquared.co.uk>
2024-07-30 20:44:42 +0100

    Fix type errors

--- frontend/new/src/main.tsx
+++ frontend/new/src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import './styles/globals.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
-);
+const rootElement = document.getElementById('root');
+
+if (rootElement) {
+	const root = createRoot(rootElement);
+	root.render(
+		<StrictMode>
+			<App />
+		</StrictMode>
+	);
+} else {
+	console.error('Root element not found');
+}
