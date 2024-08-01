ad3731ab68659bffcd2a76766ab5ab907725e490
Alex Jenkinson <alexjenkinson@ideasquared.co.uk>
2024-08-01 20:09:38 +0100

    Expanding navbar to help with testing

--- frontend/new/src/components/Navbar/Navbar.tsx
+++ frontend/new/src/components/Navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
@@ -24,6 +24,7 @@ const NavList = styled.ul`
 
 const NavItem = styled.li`
 	margin: 0 1rem;
+	position: relative;
 `;
 
 const NavLink = styled(Link)`
@@ -37,7 +38,52 @@ const NavLink = styled(Link)`
 	}
 `;
 
+const Dropdown = styled.ul<{ isOpen: boolean }>`
+	display: ${(props) => (props.isOpen ? 'block' : 'none')};
+	position: absolute;
+	top: 100%;
+	left: 0;
+	background-color: ${(props) => props.theme.background.content};
+	border: 1px solid ${(props) => props.theme.border.content};
+	list-style-type: none;
+	padding: 0.5rem 0;
+	margin: 0;
+`;
+
+const DropdownItem = styled.li`
+	margin: 0;
+`;
+
+const DropdownLink = styled(Link)`
+	display: block;
+	padding: 0.5rem 1rem;
+	color: ${(props) => props.theme.text.body};
+	text-decoration: none;
+
+	&:hover {
+		background-color: ${(props) => props.theme.background.highlight};
+		color: ${(props) => props.theme.text.highlight};
+	}
+`;
+
+const AccountLink = styled.span`
+	color: ${(props) => props.theme.text.body};
+	font-weight: bold;
+	cursor: pointer;
+
+	&:hover {
+		color: ${(props) => props.theme.text.highlight};
+		text-decoration: underline;
+	}
+`;
+
 const Navbar: React.FC = () => {
+	const [dropdownOpen, setDropdownOpen] = useState(false);
+
+	const toggleDropdown = () => {
+		setDropdownOpen(!dropdownOpen);
+	};
+
 	return (
 		<StyledNavbar>
 			<Nav>
@@ -45,20 +91,27 @@ const Navbar: React.FC = () => {
 					<NavItem>
 						<NavLink to='/'>Home</NavLink>
 					</NavItem>
-					<NavItem>
-						<NavLink to='/users'>Users</NavLink>
-					</NavItem>
-					<NavItem>
-						<NavLink to='/login'>Login</NavLink>
-					</NavItem>
-					<NavItem>
-						<NavLink to='/create-account'>Create Account</NavLink>
-					</NavItem>
-					<NavItem>
-						<NavLink to='/forgot-password'>Forgot Password</NavLink>
-					</NavItem>
-					<NavItem>
-						<NavLink to='/reset-password'>Reset Password</NavLink>
+					<NavItem onMouseEnter={toggleDropdown} onMouseLeave={toggleDropdown}>
+						<AccountLink>Account</AccountLink>
+						<Dropdown isOpen={dropdownOpen}>
+							<DropdownItem>
+								<DropdownLink to='/login'>Login</DropdownLink>
+							</DropdownItem>
+							<DropdownItem>
+								<DropdownLink to='/create-account'>Create Account</DropdownLink>
+							</DropdownItem>
+							<DropdownItem>
+								<DropdownLink to='/forgot-password'>
+									Forgot Password
+								</DropdownLink>
+							</DropdownItem>
+							<DropdownItem>
+								<DropdownLink to='/reset-password'>Reset Password</DropdownLink>
+							</DropdownItem>
+							<DropdownItem>
+								<DropdownLink to='/settings'>Settings</DropdownLink>
+							</DropdownItem>
+						</Dropdown>
 					</NavItem>
 				</NavList>
 			</Nav>
