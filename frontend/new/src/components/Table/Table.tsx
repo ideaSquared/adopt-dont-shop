42a719255bfbb55b0c78b47ac0c20ef2ec31f91c
Alex Jenkinson <alexjenkinson@ideasquared.co.uk>
2024-08-06 19:57:34 +0100

    Allow children to pass to table

--- frontend/new/src/components/Table/Table.tsx
+++ frontend/new/src/components/Table/Table.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import styled from 'styled-components';
 
 interface TableProps {
-	headers: string[];
-	data: (string | number)[][];
+	children: ReactNode;
 }
 
 const StyledTable = styled.table`
@@ -13,46 +12,8 @@ const StyledTable = styled.table`
 	border-collapse: collapse;
 `;
 
-const StyledTableHead = styled.thead`
-	background-color: #f8f9fa;
-`;
-
-const StyledTableRow = styled.tr``;
-
-const StyledTableHeader = styled.th`
-	padding: 0.75rem;
-	border: 1px solid #dee2e6;
-	text-align: left;
-	vertical-align: bottom;
-`;
-
-const StyledTableCell = styled.td`
-	padding: 0.75rem;
-	border: 1px solid #dee2e6;
-	vertical-align: top;
-`;
-
-const Table: React.FC<TableProps> = ({ headers, data }) => {
-	return (
-		<StyledTable>
-			<StyledTableHead>
-				<StyledTableRow>
-					{headers.map((header, index) => (
-						<StyledTableHeader key={index}>{header}</StyledTableHeader>
-					))}
-				</StyledTableRow>
-			</StyledTableHead>
-			<tbody>
-				{data.map((row, rowIndex) => (
-					<StyledTableRow key={rowIndex}>
-						{row.map((cell, cellIndex) => (
-							<StyledTableCell key={cellIndex}>{cell}</StyledTableCell>
-						))}
-					</StyledTableRow>
-				))}
-			</tbody>
-		</StyledTable>
-	);
+const Table: React.FC<TableProps> = ({ children }) => {
+	return <StyledTable>{children}</StyledTable>;
 };
 
 export default Table;
