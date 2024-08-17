0ddf514d1a9ac1ca8104ee79d8e63d3f97ae92c2
Alex Jenkinson <alexjenkinson@ideasquared.co.uk>
2024-08-17 14:34:00 +0100

    eslint updates

--- frontend/new/.eslintrc.cjs
+++ frontend/new/.eslintrc.cjs
@@ -1,18 +1,43 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
-}
+	root: true,
+	env: {
+		browser: true,
+		es2020: true,
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:react/recommended',
+		'plugin:react-hooks/recommended',
+		'plugin:jsx-a11y/recommended',
+		'plugin:prettier/recommended', // Integrates Prettier for code formatting
+	],
+	ignorePatterns: ['dist', '.eslintrc.cjs'],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 2020,
+		sourceType: 'module',
+	},
+	plugins: ['react-refresh', 'jsx-a11y', 'prettier'],
+	settings: {
+		react: {
+			version: 'detect', // Automatically detects the React version
+		},
+	},
+	rules: {
+		'react-refresh/only-export-components': [
+			'warn',
+			{ allowConstantExport: true },
+		],
+		'@typescript-eslint/explicit-module-boundary-types': 'off',
+		'prettier/prettier': 'warn', // Enforces Prettier formatting
+		'react/prop-types': 'off', // Since you're using TypeScript for type checking
+		'jsx-a11y/no-autofocus': 'warn', // Accessibility rule, adjust as needed
+		'no-const-assignment': 'warn',
+		'no-unused-vars': 'error',
+		'default-case': 'error',
+	},
+};
