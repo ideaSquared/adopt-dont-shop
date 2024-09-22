4e63daa072f4d5755024e538a92daa9d4fd1fd42
Alex Jenkinson <alexjenkinson@ideasquared.co.uk>
2024-09-22 15:57:46 +0100

    Fixed aut route vulns

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,14 +1,14 @@
 // src/routes/authRoutes.ts
 import express from 'express'
 import {
-  changePasswordHandler,
-  createUserAccount,
-  forgotPasswordHandler,
-  login,
-  resetPasswordHandler,
-  updateUser,
-  verifyEmail,
-  getAllUsers,
+  changePasswordController,
+  createUserAccountController,
+  forgotPasswordController,
+  getAllUsersController,
+  loginController,
+  resetPasswordController,
+  updateUserController,
+  verifyEmailController,
 } from '../controllers/userController'
 import { authenticateJWT } from '../middleware/authMiddleware'
 import { checkUserRole } from '../middleware/roleCheckMiddleware'
@@ -16,27 +16,32 @@ import { checkUserRole } from '../middleware/roleCheckMiddleware'
 const router = express.Router()
 
 // POST /api/auth/login
-router.post('/login', login)
+router.post('/login', loginController)
 
 // GET /api/auth/verify-email
-router.get('/verify-email', verifyEmail)
+router.get('/verify-email', verifyEmailController)
 
 // PUT /api/users/:userId
-router.put('/users/:userId', authenticateJWT, updateUser)
+router.put('/users/:userId', authenticateJWT, updateUserController)
 
 // PUT /api/users/:userId/change-password
 router.put(
   '/users/:userId/change-password',
   authenticateJWT,
-  changePasswordHandler,
+  changePasswordController,
 )
 
-router.post('/forgot-password', forgotPasswordHandler)
-router.post('/reset-password', resetPasswordHandler)
+router.post('/forgot-password', forgotPasswordController)
+router.post('/reset-password', resetPasswordController)
 
 // Route for creating a user account
-router.post('/create-user', createUserAccount)
+router.post('/create-user', createUserAccountController)
 
-router.get('/users', authenticateJWT, checkUserRole('admin'), getAllUsers)
+router.get(
+  '/users',
+  authenticateJWT,
+  checkUserRole('admin'),
+  getAllUsersController,
+)
 
 export default router
